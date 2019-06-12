86d9cde06e0ba4d99e928b26459990dcaab4e107
hagerhussein <hagarelrashidy@hotmail.com>
2019-06-12 14:41:46 +0200

    signup form

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -4,11 +4,11 @@ export default function SignupForm(props) {
   return (<form onSubmit={props.onSubmit}>
     <label>
       First Name:
-    <input type="text" name="First Name" value={props.value.firstName} onChange={props.onChange} />
+    <input type="text" name="firstName" value={props.values.firstName} onChange={props.onChange} />
     </label>
     <label>
       Last Name:
-    <input type="text" name="Last Name" value={props.value.lastName} onChange={props.onChange} />
+    <input type="text" name="lastName" value={props.values.lastName} onChange={props.onChange} />
     </label>
     <label>
       Email:
